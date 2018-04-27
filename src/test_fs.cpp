0710420c8478a2d7d516505d43f1e278d8e217c6
Thomas Ferrand <a.tom.hic3@gmail.com>
2018-04-27 11:31:36 +0200

    Test filesystem

---
+++ src/test_fs.cpp
@@ -0,0 +1,11 @@
+#include <experimental/filesystem>
+#include <iostream>
+
+namespace fs = std::experimental::filesystem;
+
+int main()
+{
+    std::cout << fs::current_path().string() << std::endl;
+    
+    return 0;
+}
