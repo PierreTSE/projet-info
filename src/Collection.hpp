75072af0c4faec4f5c7ddc4fea39f9f7308c6610
Thomas Ferrand <a.tom.hic3@gmail.com>
2018-05-03 16:07:33 +0200

    Collection minimale

--- src/Collection.hpp
+++ src/Collection.hpp
@@ -7,84 +7,49 @@
 template <typename T>
 class Collection
 {
-public:
-	using value_type = T;
-	using allocator_type = std::allocator<T>;
-	using reference = T&;
-	using const_reference = const T&;
-	using pointer = T*;
-	using const_pointer = const T*;
-	using iterator = CollectionIterator<T, false>;
-	using const_iterator = CollectionIterator<T, true>;
-	using reverse_iterator = std::reverse_iterator<iterator>;
-	using const_reverse_iterator = std::reverse_iterator<const_iterator>;
-	using difference_type = std::ptrdiff_t;
-	using size_type = size_t;
-
-	Collection() = default;
-	virtual ~Collection() = default;
-
-	//Assign
-	virtual void assign(size_type count, const T& value) = 0;
-	virtual void assign(iterator first, iterator last) = 0;
-	virtual void assign(std::initializer_list<T> ilist) = 0;
-
-	//Element access
-	virtual reference at(size_type pos) = 0;
-	virtual const_reference at(size_type pos) const = 0;
-	virtual reference front() = 0;
-	virtual const_reference front() const = 0;
-	virtual reference back() = 0;
-	virtual const_reference back() const = 0;
-
-	//Iterators
-	virtual iterator begin() = 0;
-	virtual const_iterator cbegin() const = 0;
-	virtual reverse_iterator rbegin() = 0;
-	virtual const_reverse_iterator rbegin() const = 0;
-	virtual const_reverse_iterator crbegin() const = 0;
-	virtual iterator end() = 0;
-	virtual const_iterator cend() const = 0;
-	virtual reverse_iterator rend() = 0;
-	virtual const_reverse_iterator rend() const = 0;
-	virtual const_reverse_iterator crend() const = 0;
-
-	//Capacity
-	virtual bool empty() const = 0;
-	virtual size_type size() const = 0;
-	virtual size_type max_size() const = 0;
-	virtual void reserve(size_type new_cap) = 0;
-	virtual size_type capacity() const = 0;
-	virtual void shrink_to_fit() = 0;
-
-	//Modifiers
-	virtual void clear() = 0;
-	virtual iterator insert(iterator pos, const T& value) = 0;
-	virtual iterator insert(const_iterator pos, const T& value) = 0;
-	virtual iterator insert(const_iterator pos, T&& value) = 0;
-	virtual void insert(iterator pos, size_type count, const T& value) = 0;
-	virtual iterator insert(const_iterator pos, size_type count, const T& value) = 0;
-	template <class InputIt>
-	virtual void insert(iterator pos, InputIt first, InputIt last) = 0;
-	template <class InputIt>
-	virtual iterator insert(const_iterator pos, InputIt first, InputIt last) = 0;
-	virtual iterator insert(const_iterator pos, std::initializer_list<T> ilist) = 0;
-	template <class... Args>
-	virtual iterator emplace(const_iterator pos, Args&&... args) = 0;
-	virtual iterator erase(iterator pos) = 0;
-	virtual iterator erase(const_iterator pos) = 0;
-	virtual iterator erase(iterator first, iterator last) = 0;
-	virtual iterator erase(const_iterator first, const_iterator last) = 0;
-	virtual void push_back(const T& value) = 0;
-	virtual void push_back(T&& value) = 0;
-	template <class... Args>
-	virtual void emplace_back(Args&&... args) = 0;
-	template <class... Args>
-	virtual reference emplace_back(Args&&... args) = 0;
-	virtual void pop_back() = 0;
-	virtual void resize(size_type count, T value = T()) = 0;
-	virtual void resize(size_type count) = 0;
-	virtual void resize(size_type count, const value_type& value) = 0;
+	public:
+		using value_type = T;
+		using allocator_type = std::allocator<T>;
+		using reference = T&;
+		using const_reference = const T&;
+		using pointer = T*;
+		using const_pointer = const T*;
+		using iterator = CollectionIterator<T, false>;
+		using const_iterator = CollectionIterator<T, true>;
+		using reverse_iterator = std::reverse_iterator<iterator>;
+		using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+		using difference_type = std::ptrdiff_t;
+		using size_type = size_t;
+	
+		Collection() = default;
+		virtual ~Collection() = default;
+	
+		//Assign
+		virtual void assign(size_type count, const T& value) = 0;
+		virtual void assign(iterator first, iterator last) = 0;
+		virtual void assign(std::initializer_list<T> ilist) = 0;
+	
+		//Element access
+		virtual reference at(size_type pos) = 0;
+		virtual const_reference at(size_type pos) const = 0;
+		virtual reference front() = 0;
+		virtual const_reference front() const = 0;
+		virtual reference back() = 0;
+		virtual const_reference back() const = 0;
+	
+		//Iterators
+		virtual iterator begin() = 0;
+		/*virtual const_iterator cbegin() const = 0;
+		virtual reverse_iterator rbegin() = 0;
+		virtual const_reverse_iterator rbegin() const = 0;
+		virtual const_reverse_iterator crbegin() const = 0;*/
+		virtual iterator end() = 0;
+		/*virtual const_iterator cend() const = 0;
+		virtual reverse_iterator rend() = 0;
+		virtual const_reverse_iterator rend() const = 0;
+		virtual const_reverse_iterator crend() const = 0;*/
+	
+		
 };
 
 #endif // !COLLECTION_HPP
--- src/CollectionPool.hpp
+++ src/CollectionPool.hpp
@@ -14,9 +14,45 @@ class CollectionPool : public Collection<T>
 {
     public:
         CollectionPool() = default;
-        CollectionPool(size_type n, const value_type& v = value_type{}) : pool_(n, v) {}
+        explicit CollectionPool(size_type n, const value_type& v = value_type{}) : pool_(n, v) {}
         CollectionPool(std::initializer_list<value_type> list) : pool_{list} {}
-        
+
+        //Capacity
+        bool empty() const { return pool_.empty(); }
+        size_type size() const { return pool_.size(); }
+        size_type max_size() const { return pool_.max_size(); }
+        void reserve(size_type new_cap) { pool_.reserve(new_cap); }
+        size_type capacity() const { return pool_.capacity(); }
+        void shrink_to_fit() { pool_.shrink_to_fit(); }
+    /*
+        //Modifiers
+        void clear() { pool_.clear(); }
+        iterator insert(const_iterator pos, const T& value) { return pool_.insert(pos, value); }
+        iterator insert(const_iterator pos, T&& value);
+        void insert(iterator pos, size_type count, const T& value);
+        iterator insert(const_iterator pos, size_type count, const T& value);
+        template <class InputIt>
+        void insert(iterator pos, InputIt first, InputIt last);
+        template <class InputIt>
+        iterator insert(const_iterator pos, InputIt first, InputIt last);
+        iterator insert(const_iterator pos, std::initializer_list<T> ilist);
+        template <class... Args>
+        iterator emplace(const_iterator pos, Args&&... args);
+        iterator erase(iterator pos);
+        iterator erase(const_iterator pos);
+        iterator erase(iterator first, iterator last);
+        iterator erase(const_iterator first, const_iterator last);
+        void push_back(const T& value);
+        void push_back(T&& value);
+        template <class... Args>
+        void emplace_back(Args&&... args);
+        template <class... Args>
+        reference emplace_back(Args&&... args);
+        void pop_back();
+        void resize(size_type count, T value = T());
+        void resize(size_type count);
+        void resize(size_type count, const value_type& value);
+        */
     
     private:
         std::vector<value_type> pool_;
