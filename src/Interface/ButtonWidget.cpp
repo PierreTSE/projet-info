352b94f4023a3b36736c67559372c33114fb9dc7
PierreTSE <pierre--giraud@orange.fr>
2018-06-02 18:11:43 +0200

    fin bouton

--- src/Interface/ButtonWidget.cpp
+++ src/Interface/ButtonWidget.cpp
@@ -1,13 +1,35 @@
 #include "ButtonWidget.hpp"
 #include <iostream>
 
+ButtonWidget::ButtonWidget(Widget& content, const dim_t& size, const std::string& text) : 
+    content_{ &content },
+    size_{ size },
+    text_{text}
+{
+	if (size.x < 13 * fontSize_)
+		size_.x = 13 * fontSize_ + 1;
+	content_->resize(size_);
+    content_->setParent(this);
+}
 
 ButtonWidget::img ButtonWidget::actualRender() const
 {
-	img render(size_.x, size_.y, 1, 3, 200);
-	size_t fontHeight = 13;
-	const unsigned char white[] = { 255, 255,255 }, grey[] = { 128,128,128 };
-	render.draw_text(1, size_.y / 2 - fontHeight, static_cast<const char* const>("mon bouton"), white, grey, 100, fontHeight);
+	const unsigned char white[] = { 255, 255, 255 }, grey[] = { 128,128,128 }, backblue[] = { 102, 153, 255 }, black[] = { 0, 0, 0 };
+    img render(size_.x, size_.y, 1, 3, 0);
+    
+	const unsigned char testColor[] = { 138, 0, 230 };
+
+    if(is_hovered && !is_clicked)
+    {
+        //TODO couleur de police		
+        //render.draw_text(1, size_.y / 2 - fontSize_, static_cast<const char* const>(text_.c_str()), testColor, backblue, 100, fontSize_);
+        //HACK pas de hover
+		render.draw_text(1, size_.y / 2 - fontSize_, static_cast<const char* const>(text_.c_str()), black, white, 100, fontSize_);
+    }
+	else
+	{
+		render.draw_text(1, size_.y / 2 - fontSize_, static_cast<const char* const>(text_.c_str()), black, white, 100, fontSize_);
+	}
 
 	return render;
 }
@@ -16,30 +38,54 @@ void ButtonWidget::actualResize(const dim_t & size)
 {
 	size_ = size;
 	content_->resize(size);
-	needRedraw();
+	callRedraw();
 }
 
 bool ButtonWidget::actualPropagateEvent(const Event& event)
 {
-	if (content_->isInside(event.first))
+	if (content_->isInside(event.pos))
 		if (content_->propagateEvent(event))
 			return true;
 
-	if (std::holds_alternative<ClickEvent>(event.second))
+	if (std::holds_alternative<ClickEvent>(event.event))
 	{
-        if(std::get<ClickEvent>(event.second).type == ClickEvent::LEFT)
-        {
-			std::cerr << "thomas est left méchant" << std::endl;
-        }
-        else if (std::get<ClickEvent>(event.second).type == ClickEvent::RIGHT)
+		if (isInside(event.pos))
 		{
-			std::cerr << "thomas est right méchant" << std::endl;
+
+			if (std::get<ClickEvent>(event.event).type == ClickEvent::LEFT)
+			{
+				std::cerr << "thomas est left méchant" << std::endl;
+				is_clicked = true;
+				callRedraw();
+				content_->propagateEvent(event);
+				return true;
+			}
+			else if (std::get<ClickEvent>(event.event).type == ClickEvent::RIGHT)
+			{
+				std::cerr << "thomas est right méchant" << std::endl;
+			}
+			else
+			{
+				std::cerr << "thomas est middle méchant" << std::endl;
+			}
+			return true;
 		}
-        else
-        {
-            std::cerr << "thomas est middle méchant" << std::endl;
-        }
-		return true;
+		else
+		{
+			is_clicked = false;
+			return false;
+		}
 	}
+	if (std::holds_alternative<MoveEvent>(event.event))
+	{
+		if (isInside(event.pos))
+		{
+			is_hovered = true;
+			callRedraw();
+		}
+		else
+			is_hovered = false;
+	}
+
 	return false;
 }
--- src/Interface/ButtonWidget.hpp
+++ src/Interface/ButtonWidget.hpp
@@ -2,22 +2,26 @@
 #define BUTTONWIDGET_HPP
 
 #include "Widget.hpp"
+#include <string>
 
 
 class ButtonWidget : public Widget
 {
     public:
-		ButtonWidget(Widget* content, dim_t size) : content_{ content }, size_{ size } { content_->resize(size_); content_->setParent(this); }
-
-    private:
+		ButtonWidget(Widget& content, const dim_t& size, const std::string& text);
+    
+    protected:
 		img actualRender() const override;
 		void actualResize(const dim_t& size) override;
 		dim_t actualSize() const override { return size_; }
 		bool actualPropagateEvent(const Event& event) override;
 
+    private:
 		dim_t size_;
 		Widget* content_ = nullptr;
-        
-
+		bool is_hovered = false;
+		bool is_clicked = false;
+		std::string text_;
+		const size_t fontSize_ = 13;
 };
 #endif // BUTTONWIDGET_HPP
